6c49e9e21e385539edfc1efb593619c6d3dc2a96
Oseme Odigie <oseme.odigie@solace.com>
2025-09-12 11:53:37 -0400

    testing the test fixtures path

--- test/constants/test_constants.go
+++ test/constants/test_constants.go
@@ -18,10 +18,10 @@
 package constants
 
 // ValidFixturesPath constant
-const ValidFixturesPath = "./data/fixtures"
+const ValidFixturesPath = "/Users/loadbuild/jenkins/slave2/workspace/ev.pubsubplus-go-client_EBP-1222_2/test/data/fixtures" // "./data/fixtures"
 
 // InvalidFixturesPath constant
-const InvalidFixturesPath = "./data/invalid_fixtures"
+const InvalidFixturesPath = "/Users/loadbuild/jenkins/slave2/workspace/ev.pubsubplus-go-client_EBP-1222_2/test/data/invalid_fixtures" // "./data/invalid_fixtures"
 
 // ValidClientCertificatePEM to install to the server for valid client authentication
 const ValidClientCertificatePEM = ValidFixturesPath + "/public_root_ca.crt"
